08479eed97efded6db44ac959c2f400e29300780
brets98 <brettaluca@gmail.com>
2019-10-01 00:01:54 +0200

    https prova 2

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ var http = require('https'),
     fs = require('fs');
 
 
+
+    const options = {
+        key: fs.readFileSync('key.pem'),
+        cert: fs.readFileSync('cert.pem')
+      };
+
+
+
+
 fs.readFile('./mappa.html', function (err, html) {
     if (err) {
         throw err; 
     }       
-    http.createServer(function(request, response) {  
+    https.createServer(options, function (req, res) {
         response.writeHeader(200, {"Content-Type": "text/html"});  
         response.write(html);  
         response.end();  
